a6b17de77532b85c405cc6ed3e7e06656d3f41cc
Robert Eastman <robert.e.eastman.10@alum.dartmouth.org>
2013-11-17 12:53:34 -0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Annotext::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # match '/signup',  to: 'users#new',    via: 'get'
 
 
