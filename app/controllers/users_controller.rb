2390fd8f693e2b4c31425a6e963601dbcc0e6328
Robert Eastman <robert.e.eastman.10@alum.dartmouth.org>
2013-11-19 00:23:04 -0600

    Finish user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user
+  before_action :correct_user,  only: [:edit, :update]
+  before_action :admin_user,    only: [:destroy, :new, :create]
+
   def new
     @user = User.new
   end
@@ -17,12 +21,56 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user?(@user)
+      redirect_to(root_url)
+    else
+      @user.destroy
+      flash[:success] = "User deleted."
+      redirect_to users_path
+    end
+  end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,24 +4,76 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe "pagination" do
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+
+      it "should list each user" do
+        User.paginate(page: 1, per_page: 20).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
+
   describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }   
-    before { visit user_path(user) }
+    let(:user) { FactoryGirl.create(:user) } 
+    before do
+      sign_in user
+      visit user_path(user)
+    end
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
 
-  describe "new user page" do
-    before { visit new_user_path }
-
-    it { should have_content('Create New User') }
-    it { should have_title(full_title('New User')) }
-  end
-
   describe "creating new user" do
+    let(:admin) { FactoryGirl.create(:admin) }
+    before do
+      sign_in admin
+      visit new_user_path
+    end
 
-    before { visit new_user_path }
+    describe "new user page" do
+      it { should have_content('Create New User') }
+      it { should have_title(full_title('New User')) }
+    end
 
     let(:submit) { "Create an account" }
 
@@ -52,11 +104,61 @@ describe "User pages" do
 
       describe "after saving the user" do
         before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
+        let(:new_user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'success') }
+        it { should have_title(new_user.name) }
+        it { should have_success_message('success') }
       end
+    end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_success_message }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 
